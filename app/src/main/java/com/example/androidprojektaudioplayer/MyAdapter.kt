c5410fba45b5cf8b001a75decfd67a4f3602fd55
Matthias Utrata <matthias.utrata@outlook.com>
2026-02-11 14:58:08 +0100

    Adapter begonnen. Muss noch fertiggestellt werden

---
+++ app/src/main/java/com/example/androidprojektaudioplayer/MyAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.androidprojektaudioplayer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+//class MyAdapter(private val audioList: MutableList<myAudio>,
+//    private val contextExt: Context
+//): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.audiocard, parent, false);
+//        return MyViewHolder(view, contextExt);
+//    }
+//}
+
+class MyViewHolder(itemView: View, contextExt: Context): RecyclerView.ViewHolder(itemView) {
+    //TODO:Adapter fertigstellen
+}
