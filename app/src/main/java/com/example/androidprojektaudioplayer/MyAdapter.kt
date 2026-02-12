eb1548a20af55772a1d5b6ec6502eeaa0c383ed4
Matthias Utrata <matthias.utrata@outlook.com>
2026-02-12 06:54:35 +0100

    Adapter fertiggestellt

--- app/src/main/java/com/example/androidprojektaudioplayer/MyAdapter.kt
+++ app/src/main/java/com/example/androidprojektaudioplayer/MyAdapter.kt
@@ -4,17 +4,32 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-//class MyAdapter(private val audioList: MutableList<myAudio>,
-//    private val contextExt: Context
-//): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.audiocard, parent, false);
-//        return MyViewHolder(view, contextExt);
-//    }
-//}
+class MyAdapter(private val audioList: MutableList<myAudio>,
+    private val contextExt: Context
+): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.audiocard, parent, false);
+        return MyViewHolder(view, contextExt);
+    }
 
-class MyViewHolder(itemView: View, contextExt: Context): RecyclerView.ViewHolder(itemView) {
-    //TODO:Adapter fertigstellen
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.itemView.isLongClickable = true;
+        val currentTrack = audioList[position];
+    }
+
+    override fun getItemCount(): Int {
+        return audioList.size;
+    }
+
+    class MyViewHolder(itemView: View, contextExt: Context): RecyclerView.ViewHolder(itemView) {
+        val tvTitle: TextView = itemView.findViewById(R.id.tvTitle);
+        val tvGenre: TextView = itemView.findViewById(R.id.tvGenre);
+        val tvArtist: TextView = itemView.findViewById(R.id.tvArtist);
+        val tvDate: TextView = itemView.findViewById(R.id.tvDate);
+        val card: View = itemView.findViewById(R.id.songID);
+    }
 }
+
